c53cca7114c4d3c68578af1dd99bbb6b7be17ad1
wll8 <xwdaysme@gmail.com>
2021-04-18 17:04:05 +0800

    feat: 添加 mockm
    使用 `npm run mm` 启动 mockm

---
+++ mm.config.js
@@ -0,0 +1,18 @@
+/**
+ * 文档: https://www.hongqiye.com/doc/mockm
+ */
+
+module.exports = util => {
+  const {
+    libObj: { midResJson, axios, mime, mockjs },
+    toolObj,
+  } = util
+  return {
+    osIp: `127.0.0.1`,
+    port: 9000,
+    testPort: 9005,
+    replayPort: 9001,
+    apiWeb: './apiWeb.json',
+    guard: true,
+  }
+}
