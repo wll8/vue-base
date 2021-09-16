182f0f7f65da3139582ff98a4e23926760601ca7
wll8 <xwdaysme@gmail.com>
2021-09-16 13:16:49 +0800

    feat: 优化 mm 配置

--- mm/api/index.js
+++ mm/api/index.js
@@ -1,16 +1,41 @@
-const { wrapApiData } = require(`../util.js`)
+/**
+ * 类型定义
+ * @typedef {import('mockm/@types/config').Config} Config
+ * @typedef {import('mockm/@types/config').Api} Api
+ */
 
-module.exports = util => {
+const { dateFormat, wrapApiData } = require(`../util.js`)
+
+/** @type Config */
+module.exports = (util) => {
   const {
     libObj: { mockjs },
+    toolObj,
   } = util
+  const api = {
+    // 覆盖已存在的接口
+    /** @type Api */
+    cover: {},
+    // 模拟不存在的接口
+    /** @type Api */
+    nonExistent: {
+      // 创建接口并使用 mockjs 生成数据
+      'get /api/test': wrapApiData(
+        mockjs.mock({
+          'data|3-7': [
+            {
+              userId: `@id`,
+              userName: `@cname`,
+            },
+          ],
+        })
+      ),
+    },
+  }
   return {
-    // 创建接口并使用 mockjs 生成数据
-    'get /api/test': wrapApiData(mockjs.mock({
-      'data|3-7': [{
-        userId: `@id`,
-        userName: `@cname`,
-      }]
-    })),
+    api: {
+      // ...api.cover,
+      ...api.nonExistent,
+    },
   }
 }
--- mm/mm.config.js
+++ mm/mm.config.js
@@ -1,12 +1,18 @@
-/**
- * 配置说明请参考文档: 
- * https://hongqiye.com/doc/mockm/config/option.html
- */
-
 const api = require(`./api/index.js`)
-const {wrapApiData} = require(`./util.js`)
+const { wrapApiData } = require(`./util.js`)
 
-module.exports = util => {
+/**
+ * 配置说明请参考文档:
+ * https://hongqiye.com/doc/mockm/config/option.html
+ * @type {import('mockm/@types/config').Config}
+ */
+module.exports = (util) => {
+  setTimeout(() => {
+    // 定时清除缓存
+    const fs = require(`fs`)
+    fs.unlinkSync(`${__dirname}/httpData/httpHistory.json`)
+    fs.writeFileSync(__filename, fs.readFileSync(__filename))
+  }, 30 * 60 * 1000)
   return {
     guard: false,
     port: 9000,
@@ -19,7 +25,7 @@ module.exports = util => {
       '/anything/intercept': [`origin`, `127.0.0.1`],
     },
     api: {
-      ...api(util),
+      ...api(util).api,
       '/api/user': {
         success: true,
         data: {
